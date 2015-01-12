f16272f547ae05089080710b718086e97eeb3b6e
Stuart Hinson <stuart.hinson@gmail.com>
2015-01-12 10:23:15 -0500

    include tools.reader

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
                  [compojure "1.3.1"]
                  [ring/ring-defaults "0.1.2"]
                  [hiccup "1.0.5"]
-                 [org.clojure/tools.logging "0.3.1"]]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.clojure/tools.reader "0.8.13"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler todo.core.handler/app
          :nrepl {:start? true}}
